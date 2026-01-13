e993eb0b855fad56ccddc3ddef1f592a5c050725
Yorgi De Schrijver <yorgi.deschrijver@student.ap.be>
2026-01-13 15:14:45 +0100

    added header design

--- app/routes.ts
+++ app/routes.ts
@@ -1,5 +1,5 @@
 import { type RouteConfig, index, layout } from '@react-router/dev/routes';
 
 export default [
-  layout('routers/_layout.tsx', [index('routes/index.tsx')]),
+  layout('routes/_layout.tsx', [index('routes/_index.tsx')]),
 ] satisfies RouteConfig;
