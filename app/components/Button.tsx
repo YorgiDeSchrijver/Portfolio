92f29bbed7fcd71f9bf493051340d22f13d0b757
Yorgi De Schrijver <yorgi.deschrijver@student.ap.be>
2026-01-13 15:24:54 +0100

    copied initial design

---
+++ app/components/Button.tsx
@@ -0,0 +1,51 @@
+import { cva, type VariantProps } from "class-variance-authority";
+import { ArrowRight } from "lucide-react";
+import { Link } from "react-router";
+
+const textVariants = cva("", {
+  variants: {
+    size: {
+      default: "px-36 py-4 text-2xl",
+      md: "px-16 py-3.5 text-xl",
+      sm: "px-16 py-2 text-lg",
+    },
+  },
+  defaultVariants: {
+    size: "default",
+  },
+});
+
+const iconVariants = cva("", {
+  variants: {
+    iconSize: {
+      default: "p-4",
+      md: "p-3",
+      sm: "p-2",
+    },
+  },
+  defaultVariants: {
+    iconSize: "default",
+  },
+});
+
+interface ButtonProps
+  extends VariantProps<typeof textVariants>,
+    VariantProps<typeof iconVariants> {
+  href: string;
+  children: React.ReactNode;
+}
+
+export default function Button({ href, children, size, iconSize }: ButtonProps) {
+  const textClass = textVariants({ size });
+  const iconClass = iconVariants({ iconSize });
+  return (
+    <Link to={href} className="flex flex-row items-end gap-3 justify-end">
+      <span className={`bg-light text-dark text-nowrap rounded-full italic ${textClass}`}>
+        {children}
+      </span>
+      <span className={`bg-light rounded-full text-dark ${iconClass}`}>
+        <ArrowRight size={32} strokeWidth={1.5} />
+      </span>
+    </Link>
+  );
+}
---
+++ app/components/ButtonSocial.tsx
@@ -0,0 +1,19 @@
+import { cloneElement, type ReactNode } from "react";
+
+interface HeadLinkProps {
+  href: string;
+  icon: ReactNode;
+  text: string;
+}
+
+export default function ButtonSocial({ href, icon, text }: HeadLinkProps) {
+  return (
+    <a
+      href={href}
+      className="rounded-full gap-4 font-medium flex flex-row justify-center items-center text-xl font-sans text-gray-light border-gray-dark border py-4 px-8 italic tracking-tight bg-dark"
+    >
+      {cloneElement(icon as React.ReactElement<any>, { className: "text-light fill-light" })}
+      {text}
+    </a>
+  );
+}
