bb820b06deb7c3980ba03fa5b2349613764744a3
Yorgi De Schrijver <yorgi.deschrijver@student.ap.be>
2026-01-19 16:29:23 +0100

    fixed header

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router';
+import { Link, NavLink } from 'react-router';
 
 export default function Header({
   links,
@@ -8,15 +8,21 @@ export default function Header({
   return (
     <header className='w-full py-2 transition-all duration-1000 fixed top-0 z-9999 px-4 md:px-8 xl:px-12 2xl:px-28 bg-dark'>
       <div className='flex w-full justify-between items-center max-w-360 mx-auto '>
-        <Link to='/' className='flex flex-col text-xl w-1/3'>
+        <NavLink to='/' className='flex flex-col text-xl w-1/3'>
           <span>Yorgi</span>
           <span>De Schrijver</span>
-        </Link>
+        </NavLink>
         <nav className='flex space-x-16 items-center justify-center text-light font-medium w-1/3'>
           {links.map((link) => (
-            <Link key={link.to} to={link.to}>
-              {link.label}
-            </Link>
+            <NavLink key={link.to} to={link.to}>
+              {({ isActive }) => (
+                <span
+                  className={isActive ? 'underline underline-offset-8' : 'hover:underline underline-offset-8'}
+                >
+                  {link.label}
+                </span>
+              )}
+            </NavLink>
           ))}
         </nav>
         <div className='flex flex-col items-end text-lg w-1/3'>
